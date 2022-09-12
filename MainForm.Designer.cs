395f57f97927f5bd2646f1502cdb4d829449909d
Gabriele Bologna <bolognagabriele2003@gmail.com>
2022-09-12 14:34:14 +0200

    First commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,120 @@
+﻿partial class MainForm
+{
+    private System.ComponentModel.IContainer components = null;
+
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+
+        base.Dispose(disposing);
+    }
+
+    private void InitializeComponent()
+    {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.siticoneControlBox2 = new ns1.SiticoneControlBox();
+            this.siticoneControlBox1 = new ns1.SiticoneControlBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.gunaButton1 = new Guna.UI.WinForms.GunaButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // siticoneControlBox2
+            // 
+            this.siticoneControlBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.siticoneControlBox2.ControlBoxType = ns5.ControlBoxType.MinimizeBox;
+            this.siticoneControlBox2.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.siticoneControlBox2.HoveredState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.siticoneControlBox2.HoveredState.Parent = this.siticoneControlBox2;
+            this.siticoneControlBox2.IconColor = System.Drawing.Color.White;
+            this.siticoneControlBox2.Location = new System.Drawing.Point(168, 11);
+            this.siticoneControlBox2.Name = "siticoneControlBox2";
+            this.siticoneControlBox2.ShadowDecoration.Parent = this.siticoneControlBox2;
+            this.siticoneControlBox2.Size = new System.Drawing.Size(45, 29);
+            this.siticoneControlBox2.TabIndex = 5;
+            // 
+            // siticoneControlBox1
+            // 
+            this.siticoneControlBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.siticoneControlBox1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.siticoneControlBox1.HoveredState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.siticoneControlBox1.HoveredState.Parent = this.siticoneControlBox1;
+            this.siticoneControlBox1.IconColor = System.Drawing.Color.White;
+            this.siticoneControlBox1.Location = new System.Drawing.Point(213, 11);
+            this.siticoneControlBox1.Name = "siticoneControlBox1";
+            this.siticoneControlBox1.ShadowDecoration.Parent = this.siticoneControlBox1;
+            this.siticoneControlBox1.Size = new System.Drawing.Size(45, 29);
+            this.siticoneControlBox1.TabIndex = 4;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(64, 124);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // gunaButton1
+            // 
+            this.gunaButton1.Animated = true;
+            this.gunaButton1.AnimationHoverSpeed = 0.07F;
+            this.gunaButton1.AnimationSpeed = 0.03F;
+            this.gunaButton1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.gunaButton1.BorderColor = System.Drawing.Color.Black;
+            this.gunaButton1.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.gunaButton1.FocusedColor = System.Drawing.Color.Empty;
+            this.gunaButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.gunaButton1.ForeColor = System.Drawing.Color.White;
+            this.gunaButton1.Image = ((System.Drawing.Image)(resources.GetObject("gunaButton1.Image")));
+            this.gunaButton1.ImageSize = new System.Drawing.Size(32, 32);
+            this.gunaButton1.Location = new System.Drawing.Point(20, 66);
+            this.gunaButton1.Name = "gunaButton1";
+            this.gunaButton1.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(99)))), ((int)(((byte)(165)))));
+            this.gunaButton1.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.gunaButton1.OnHoverForeColor = System.Drawing.Color.White;
+            this.gunaButton1.OnHoverImage = null;
+            this.gunaButton1.OnPressedColor = System.Drawing.Color.Black;
+            this.gunaButton1.Size = new System.Drawing.Size(225, 42);
+            this.gunaButton1.TabIndex = 16;
+            this.gunaButton1.Text = "Start generating your TV signal";
+            this.gunaButton1.TextRenderingHint = Guna.UI.WinForms.DrawingTextRenderingHint.AntiAliasGridFit;
+            this.gunaButton1.Click += new System.EventHandler(this.gunaButton1_Click);
+            // 
+            // MainForm
+            // 
+            this.AllowResize = false;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(268, 277);
+            this.Controls.Add(this.gunaButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.siticoneControlBox2);
+            this.Controls.Add(this.siticoneControlBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlDark;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.State = MetroSuite.MetroForm.FormState.Normal;
+            this.Style = MetroSuite.Design.Style.Dark;
+            this.Text = "TV Signal Generator";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+    }
+
+    private ns1.SiticoneControlBox siticoneControlBox2;
+    private ns1.SiticoneControlBox siticoneControlBox1;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private Guna.UI.WinForms.GunaButton gunaButton1;
+}
---
+++ MainForm.cs
@@ -0,0 +1,79 @@
+﻿using MetroSuite;
+using ProtoRandom;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+public partial class MainForm : MetroForm
+{
+    private Thread currentGenerationThread;
+
+    public MainForm()
+    {
+        InitializeComponent();
+        Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.RealTime;
+    }
+
+    private void gunaButton1_Click(object sender, System.EventArgs e)
+    {
+        if (gunaButton1.Text.Equals("Start generating your TV signal"))
+        {
+            gunaButton1.Text = "Stop generating your TV signal";
+            currentGenerationThread = new Thread(GenerateTVSignal);
+            currentGenerationThread.Priority = ThreadPriority.Highest;
+            currentGenerationThread.Start();
+        }
+        else
+        {
+            currentGenerationThread.Abort();
+            gunaButton1.Text = "Start generating your TV signal";
+            pictureBox1.BackgroundImage = null;
+        }
+    }
+
+    public void GenerateTVSignal()
+    {
+        while (true)
+        {
+            ProtoRandom.ProtoRandom random = new ProtoRandom.ProtoRandom(1);
+            Bitmap bitmap = new Bitmap(128, 128);
+
+            for (int i = 0; i < 128; i++)
+            {
+                for (int j = 0; j < 128; j++)
+                {
+                    int generated = random.GetRandomInt32(0, 4);
+
+                    if (generated == 0)
+                    {
+                        bitmap.SetPixel(i, j, Color.White);
+                    }
+                    else if (generated == 1)
+                    {
+                        bitmap.SetPixel(i, j, Color.LightGray);
+                    }
+                    else if (generated == 2)
+                    {
+                        bitmap.SetPixel(i, j, Color.Gray);
+                    }
+                    else if (generated == 3)
+                    {
+                        bitmap.SetPixel(i, j, Color.DarkGray);
+                    }
+                    else if (generated == 4)
+                    {
+                        bitmap.SetPixel(i, j, Color.Black);
+                    }
+                }
+            }
+
+            pictureBox1.BackgroundImage = bitmap;
+        }
+    }
+
+    private void MainForm_FormClosing(object sender, System.Windows.Forms.FormClosingEventArgs e)
+    {
+        Process.GetCurrentProcess().Kill();
+    }
+}
